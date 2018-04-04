ef8e21a6df3f8baf1aca53b63b89c36dc42fb676
jiangtt18 <tjiang90111@gmail.com>
2018-04-04 14:15:11 -0700

    can use auth/protected routes

--- frontend/components/app.jsx
+++ frontend/components/app.jsx
@@ -4,14 +4,18 @@ import { Provider } from 'react-redux';
 import SignupContainer from './session/signup_container';
 import LoginContainer from './session/login_container';
 import NavBarContainer from './nav_bar/nav_bar_container';
+import {AuthRoute, ProtectedRoute} from '../utils/route_util';
+import{BookContianer} from './booking/book_container';
+
 
 const App = () => (
   <div>
     <header>
     </header>
     <Route path="/" component={NavBarContainer}/>
-    <Route path='/signup' component={SignupContainer} />
-    <Route path='/login' component={LoginContainer} />
+    <AuthRoute path='/signup' component={SignupContainer} />
+    <AuthRoute path='/login' component={LoginContainer} />
+    <ProtectedRoute path='/book' component={BookContianer} />
   </div>
 );
 
---
+++ frontend/components/booking/book_container.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export const BookContianer = () => {
+  return (
+      <div>
+        Booking
+      </div>
+
+  );
+};
