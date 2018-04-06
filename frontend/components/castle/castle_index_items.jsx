8af861f36e0be94949018faeec63f2b4a6fd13da
jiangtt18 <tjiang90111@gmail.com>
2018-04-06 14:08:00 -0700

    take out show redirect link for heroku push

--- frontend/components/castle/castle_index_items.jsx
+++ frontend/components/castle/castle_index_items.jsx
@@ -5,13 +5,8 @@ import { withRouter } from 'react-router-dom';
 class castleIndexItem extends React.Component {
  constructor(props) {
    super(props);
-   this.handleClick = this.handleClick.bind(this);
  }
 
- handleClick() {
-   const castleId = this.props.castles.castle.id;
-   this.props.history.push(`/castles/${castleId}`);
- }
 
  render() {
    const { city, title, price, num_guests, castleImageId} = this.props.castle;
@@ -23,16 +18,14 @@ class castleIndexItem extends React.Component {
    return (
      <div
        className="castle-index-item"
-       onClick={this.handleClick}
+
      >
        <div className="index-item-info">
          <span>{city}</span>
          <span>{title}</span>
          <span className="index-item-copy">{price} per night</span>
          <span className="index-item-category">Rating:5 {num_guests}</span>
-         {/*  <span className="index-item-copy">
-            {average_rating || 'No reviews yet'}
-          </span> */}
+
        </div>
        <img
          src={image_url}
