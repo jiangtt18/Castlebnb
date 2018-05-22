50c9bb626c7bb11b742ded9cc36a1980b348d3ef
jiangtt18 <tjiang90111@gmail.com>
2018-05-22 13:32:57 -0700

    changed footer description

--- frontend/components/footer/footer.jsx
+++ frontend/components/footer/footer.jsx
@@ -9,9 +9,9 @@ const Footer = (props) => {
         &copy; 2018 TingTing. All rights reserved.
       </small>
       <ul className='footer-links'>
-        <li><a className="footer-text" href="https://www.linkedin.com/feed/">Company</a></li>
-        <li><a className="footer-text" href="https://github.com/jiangtt18/Castlebnb">About</a></li>
-        <li><a className="footer-text" href="https://castlebnb.herokuapp.com/#/">Hosting</a></li>
+        <li><a className="footer-text" href="https://www.linkedin.com/feed/">Linkedin</a></li>
+        <li><a className="footer-text" href="https://github.com/jiangtt18/Castlebnb">Github</a></li>
+        <li><a className="footer-text" href="https://castlebnb.herokuapp.com/#/">Website</a></li>
       </ul>
     </footer>
   );
